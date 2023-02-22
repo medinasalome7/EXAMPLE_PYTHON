72f550fd5beeec6c62b13024c4ad085aa9296eb4
medinasalome7 <medinasalome1983@gmail.com>
2023-02-22 10:12:19 -0500

    Ejercicio_almacen_wc

---
+++ Ejercicio_lista.py
@@ -0,0 +1,26 @@
+#examples of list 
+nombres= ["Salome", "Valentina", "Daniel"]
+#print (nombres)
+
+#print (nombres[1])
+
+
+#datos_persona = ["Salome", 17, 1.52, True] en forma de lista 
+#Diccionario
+"""
+datos_persona = {
+    "nombre": "Salome",
+    "edad":17,
+    "estatura":1.52,
+    "soltero": True
+    }
+"""
+
+#En forma de tupla 
+datos_persona = ("Salome", 17, 1.52, True)
+#print (datos_persona[2])
+
+price = [100,250,300]
+resultado = price [0] + price [2]
+print("El cliente", datos_persona[0], "compro $", resultado)
+print(price)
